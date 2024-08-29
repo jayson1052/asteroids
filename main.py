77c2332fee51913dd929f84b0bb1acfa7a78349e
jayson1052 <jaysonng1052@gmail.com>
2024-08-28 20:21:32 -0400

    Add move() and update update() now I can move around the screen

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ Screen height: {SCREEN_HEIGHT}
                 return
         pygame.Surface.fill(screen, 'black', rect=None, special_flags=0)
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 if __name__ == "__main__":
