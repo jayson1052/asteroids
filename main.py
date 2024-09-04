598f563996dca05c26742709dc9f0f85c123de9c
jayson1052 <jaysonng1052@gmail.com>
2024-09-03 22:10:44 -0400

    Add 2 new groups and add all player instance to the groups

--- main.py
+++ main.py
@@ -13,14 +13,19 @@ Screen height: {SCREEN_HEIGHT}
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, 'black', rect=None, special_flags=0)
-        player.draw(screen)
-        player.update(dt)
+        for drawable_item in drawable:
+            drawable_item.draw(screen)
+        for data in updatable:
+            data.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 if __name__ == "__main__":
